376ea9bb8d74cf896db57de93659acc7a4a1c5e1
F4HH0oF <alekzanderignatov@gmail.com>
2023-01-23 22:06:48 +0200

    Checking if it works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Shoes</h2>
+          <p>Shop now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Women's</h2>
+          <p>Shop now</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Men's</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
     </div>
   );
 }

5c42a1ebf64c73283af5b476fc6a8dd27cc720d1
F4HH0oF <alekzanderignatov@gmail.com>
2023-01-24 10:39:37 +0200

    Added categories menu with category components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
+import CategoriesMenu from './components/categories-menu/categories-menu.component';
 const App = () => {
-  return (
-    <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Shoes</h2>
-          <p>Shop now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Women's</h2>
-          <p>Shop now</p>
-        </div>
-        <div className="category-body-container">
-          <h2>Men's</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+  return <CategoriesMenu />;
+};
 
 export default App;

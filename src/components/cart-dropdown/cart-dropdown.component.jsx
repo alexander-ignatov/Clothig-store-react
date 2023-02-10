3c8956c940240d5292b75b9ccff1978a9f4d5f8f
alekzandaryi <alekzandaryi@students.npmg.org>
2023-02-10 08:43:06 +0200

    Routed checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,8 +3,11 @@ import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import { useNavigate } from 'react-router-dom';
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const navigateCheckout = () => navigate("/checkout")
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -12,7 +15,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={navigateCheckout}>CHECKOUT</Button>
     </div>
   );
 };

0357ba2a7468c6bdc20380e0be1b124ab522afef
alekzandaryi <alekzandaryi@students.npmg.org>
2023-03-21 12:00:50 +0200

    reducer utils in ts almost done

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,9 +1,26 @@
 import { AnyAction } from 'redux';
 
-export type Matchable<AC extends () => AnyAction> = AC & {
+type Matchable<AC extends () => AnyAction> = AC & {
   type: ReturnType<AC>['type'];
   match(action: AnyAction): action is ReturnType<AC>;
-};
+}
+
+// We may recieve AC that has no params and returns AnyAction but the AnyAction's type value is always a string
+// We recieve actionCreator as the literal param for withMatcher function
+// The AC we pass is also the AC that is given to Matchable
+export function withMatcher<AC extends () => AnyAction & { type: string }>(actionCreator: AC): Matchable<AC>
+
+export function withMatcher<AC extends (...args: any[]) => AnyAction & { type: string }>(actionCreator: AC): Matchable<AC>
+
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    }
+  })
+}
 
 export type ActionWithPayload<T, P> = {
   type: T;
